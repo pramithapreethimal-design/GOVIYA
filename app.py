bdcc23d4911cfb22369a3335afdcd38141f510d0
pramithasahan <pramithasahan38@gmail.com>
2026-03-26 00:20:49 +0530

    add ssl

--- app.py
+++ app.py
@@ -56,3 +56,5 @@ if __name__ == '__main__':
         print(" Open in Browser: http://127.0.0.1:5000")
         
         app.run(host='0.0.0.0', port=5000, debug=False)
+        
+        app.config['MYSQL_SSL'] = {'ssl_mode': 'REQUIRED'}
